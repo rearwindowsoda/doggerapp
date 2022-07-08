b3e4282d77de30dde5b31d49677f538eba2d2ecc
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-08 21:23:31 +0200

    estabilished connection with mysql database with typeorm

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import * as express from 'express'
+import * as fileUpload from 'express-fileupload';
+import 'express-async-errors';
+import 'reflect-metadata';
+import {handleError} from "./utils/errors";
+import {newPostRouter} from "./routers/newpost";
+import {AppDataSource} from "./data-source";
+
+/*Initialize DB Connection*/
+AppDataSource.initialize().then(async () => {
+    const app = express();
+
+    app.use(fileUpload({
+        limits:{fileSize: 5 * 1024 * 1024},
+        abortOnLimit: true,
+        useTempFiles : true,
+        tempFileDir : 'uploads/tmp/',
+        preserveExtension: 4,
+
+    }));
+
+//Express.json middleware
+    app.use(express.json());
+
+    /*Express Router*/
+    app.use('/upload', newPostRouter);
+
+
+    /*Handling errors*/
+    app.use(handleError);
+
+
+    /* Starting application */
+    app.listen('3000', () => {
+        console.log('Listening on http://localhost:3000');
+    });
+
+}).catch(error => console.log(error))
+
