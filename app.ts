a80bc6d6258c628d3d1cba098ed7ce526cb69467
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-08 19:45:03 +0200

    estabilished connection with mysql database

--- app.ts
+++ app.ts
@@ -4,7 +4,25 @@ import 'express-async-errors';
 import 'reflect-metadata';
 import {handleError} from "./utils/errors";
 import {newPostRouter} from "./routers/newpost";
+import {createConnection} from "typeorm";
+import {dbSettings} from "./config/db";
 const app = express();
+
+(async () => {
+    try{
+        await createConnection({
+            type: "mysql",
+            host: dbSettings.host,
+            port: dbSettings.port,
+            username: dbSettings.username,
+            password: dbSettings.password,
+            database: dbSettings.database,
+        })
+        console.log('Connected to database');
+    }catch (e){
+        console.error('Unable to connect to database');
+        throw new Error('Unable to connect to database')
+    }})()
 
 app.use(fileUpload({
     limits:{fileSize: 5 * 1024 * 1024},
