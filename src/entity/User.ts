aba5615dd5d5062851dc5f9265b5e467c62610f0
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-09 01:46:00 +0200

    installed bcrypt and jsonwebtoken

---
+++ src/entity/User.ts
@@ -0,0 +1,26 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm"
+
+@Entity()
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({
+        length: 15,
+        unique: true,
+    })
+    login: string
+
+    @Column({
+        length: 60,
+        unique: true,
+    })
+    email: string
+
+    @Column()
+    password: string
+
+    @Column('datetime')
+    registeredAt: Date
+
+}
