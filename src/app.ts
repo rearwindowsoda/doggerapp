07968d3a6c6b625f049bb59c09e82a8a9c8308f2
MateuszOwczarek <mowczarek602@gmail.com>
2022-07-11 15:53:53 +0200

    Implemented CORS

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import * as fileUpload from 'express-fileupload';
 import 'express-async-errors';
 import 'reflect-metadata';
+import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser'
 import {handleError} from "./utils/errors";
 import {newPostRouter} from "./routers/newPost";
@@ -9,11 +10,19 @@ import {AppDataSource} from "./data-source";
 import {showpostsRouter} from "./routers/showPosts";
 import {userRouter} from "./routers/user";
 import {likePostRouter} from "./routers/likePost";
+import {corsConfig} from "./config/cors/cors";
 
 /*Initialize DB Connection*/
 AppDataSource.initialize().then(async () => {
     const app = express();
 
+    /*Cors*/
+    app.use(cors({
+        origin: corsConfig,
+    }))
+
+
+    /*Middlewares*/
     app.use(fileUpload({
         limits: {fileSize: 5 * 1024 * 1024},
         abortOnLimit: true,
@@ -23,10 +32,10 @@ AppDataSource.initialize().then(async () => {
 
     }));
 
-    /*Middlewares*/
     app.use(express.json());
     app.use(cookieParser())
-    app.use(handleError);
+
+
 
     /*Express Router*/
     app.use('/upload', newPostRouter);
@@ -34,10 +43,12 @@ AppDataSource.initialize().then(async () => {
     app.use('/user', userRouter);
     app.use('/post', likePostRouter);
 
+/*Error handling*/
+    app.use(handleError);
 
     /* Starting application */
-    app.listen('3000', () => {
-        console.log('Listening on http://localhost:3000');
+    app.listen('3001', () => {
+        console.log('Listening on http://localhost:3001');
     });
 
 }).catch(error => console.log(error))
